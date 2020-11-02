6141b0245619e47db7f6696d71f90101daa2ceea
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-11-02 18:19:06 -0500

    Data analysis query 7

--- Data_Analysis/Data_Analysis_Queries.sql
+++ Data_Analysis/Data_Analysis_Queries.sql
@@ -68,3 +68,17 @@ INNER JOIN employees AS e
 ON de.emp_no = e.emp_no
 WHERE dept_name = 'Sales'
 ORDER BY de.emp_no;
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT
+	de.emp_no AS "Employee Number",
+	first_name AS "First Name",
+	last_name AS "Last Name",
+	dept_name AS "Department Name"
+FROM dept_emp AS de
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+INNER JOIN employees AS e
+ON de.emp_no = e.emp_no
+WHERE dept_name IN ('Sales', 'Development') 
+ORDER BY de.emp_no;
