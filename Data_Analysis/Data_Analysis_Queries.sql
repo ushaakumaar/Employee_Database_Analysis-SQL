cc681553e5af9b7eba0a8d022e2c65ec39f31393
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-11-02 18:18:41 -0500

    Data analysis query 6

--- Data_Analysis/Data_Analysis_Queries.sql
+++ Data_Analysis/Data_Analysis_Queries.sql
@@ -55,3 +55,16 @@ FROM employees AS e
 WHERE lower(first_name) = 'hercules'
 	AND last_name LIKE 'B%';
 
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT
+	de.emp_no AS "Employee Number",
+	first_name AS "First Name",
+	last_name AS "Last Name",
+	dept_name AS "Department Name"
+FROM dept_emp AS de
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+INNER JOIN employees AS e
+ON de.emp_no = e.emp_no
+WHERE dept_name = 'Sales'
+ORDER BY de.emp_no;
