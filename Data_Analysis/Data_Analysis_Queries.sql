64337ca591fab485eea24d49a441db71241f97eb
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-11-02 18:17:59 -0500

    Data analysis query 4

--- Data_Analysis/Data_Analysis_Queries.sql
+++ Data_Analysis/Data_Analysis_Queries.sql
@@ -33,3 +33,16 @@ ON dm.dept_no = d.dept_no
 INNER JOIN employees AS e
 ON dm.emp_no = e.emp_no;
 
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT
+	de.emp_no AS "Employee Number",
+	first_name AS "First Name",
+	last_name AS "Last Name",
+	dept_name AS "Department Name"
+FROM dept_emp AS de
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+INNER JOIN employees AS e
+ON de.emp_no = e.emp_no
+ORDER BY de.emp_no;
+
