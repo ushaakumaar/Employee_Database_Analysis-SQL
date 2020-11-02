b8757d7d6d7d353915fd5dc7060eaa81d0a5a35c
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-11-02 18:17:06 -0500

    Data analysis query 2

--- Data_Analysis/Data_Analysis_Queries.sql
+++ Data_Analysis/Data_Analysis_Queries.sql
@@ -11,3 +11,11 @@ INNER JOIN salaries AS s
 ON e.emp_no = s.emp_no
 ORDER BY e.emp_no;
 
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT 
+	first_name AS "First Name",
+	last_name AS "Last Name",
+	hire_date AS "Hire Date"
+FROM employees AS e
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+ORDER BY hire_date;
