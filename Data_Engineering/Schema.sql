e53cb44731a63511b5f7619352e45ea1d18cf96f
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-11-03 16:42:51 -0500

    SQL to create the table schema for each of the six CSV files.

--- Data_Engineering/Schema.sql
+++ Data_Engineering/Schema.sql
@@ -1,27 +1,28 @@
-DROP TABLE IF EXISTS Dept_Manager CASCADE;
-DROP TABLE IF EXISTS Dept_Emp CASCADE;
-DROP TABLE IF EXISTS Salaries CASCADE;
-DROP TABLE IF EXISTS Employees CASCADE;
-DROP TABLE IF EXISTS Titles CASCADE;
-DROP TABLE IF EXISTS Departments CASCADE;
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS titles CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS salaries CASCADE;
+DROP TABLE IF EXISTS dept_emp CASCADE;
+DROP TABLE IF EXISTS dept_manager CASCADE;
 
-CREATE TABLE Departments (
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+CREATE TABLE departments (
     dept_no VARCHAR(4)   NOT NULL,
     dept_name VARCHAR(255)   NOT NULL,
-    CONSTRAINT pk_Departments PRIMARY KEY (
+    CONSTRAINT pk_departments PRIMARY KEY (
         dept_no
      )
 );
 
-CREATE TABLE Titles (
+CREATE TABLE titles (
     title_id VARCHAR(5)   NOT NULL,
     title VARCHAR(255)   NOT NULL,
-    CONSTRAINT pk_Titles PRIMARY KEY (
+    CONSTRAINT pk_titles PRIMARY KEY (
         title_id
      )
 );
 
-CREATE TABLE Employees (
+CREATE TABLE employees (
     emp_no INT   NOT NULL,
     emp_title_id VARCHAR(5)   NOT NULL,
     birth_date DATE   NOT NULL,
@@ -29,40 +30,49 @@ CREATE TABLE Employees (
     last_name VARCHAR(255)   NOT NULL,
     sex CHAR(1)   NOT NULL,
     hire_date DATE   NOT NULL,
-    CONSTRAINT pk_Employees PRIMARY KEY (
+    CONSTRAINT pk_employees PRIMARY KEY (
         emp_no
      )
 );
 
-CREATE TABLE Dept_Emp (
+CREATE TABLE salaries (
     emp_no INT   NOT NULL,
-    dept_no VARCHAR(4)   NOT NULL
+    salary INT   NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no
+     )
 );
 
-CREATE TABLE Dept_Manager (
+CREATE TABLE dept_emp (
+    emp_no INT   NOT NULL,
     dept_no VARCHAR(4)   NOT NULL,
-    emp_no INT   NOT NULL
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no,dept_no
+     )
 );
 
-CREATE TABLE Salaries (
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(4)   NOT NULL,
     emp_no INT   NOT NULL,
-    salary INT   NOT NULL
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        dept_no,emp_no
+     )
 );
 
-ALTER TABLE Employees ADD CONSTRAINT fk_Employees_emp_title_id FOREIGN KEY(emp_title_id)
-REFERENCES Titles (title_id);
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
 
-ALTER TABLE Dept_Emp ADD CONSTRAINT fk_Dept_Emp_emp_no FOREIGN KEY(emp_no)
-REFERENCES Employees (emp_no);
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
 
-ALTER TABLE Dept_Emp ADD CONSTRAINT fk_Dept_Emp_dept_no FOREIGN KEY(dept_no)
-REFERENCES Departments (dept_no);
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
 
-ALTER TABLE Dept_Manager ADD CONSTRAINT fk_Dept_Manager_dept_no FOREIGN KEY(dept_no)
-REFERENCES Departments (dept_no);
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
 
-ALTER TABLE Dept_Manager ADD CONSTRAINT fk_Dept_Manager_emp_no FOREIGN KEY(emp_no)
-REFERENCES Employees (emp_no);
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
 
-ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
-REFERENCES Employees (emp_no);
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
